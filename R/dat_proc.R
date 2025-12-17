b2f70c6f18f369e07bf09da9622049bfe9a71fbb
fawda123 <mbafs2012@gmail.com>
2025-12-17 10:27:33 -0500

    adding cumulative lagged models

--- R/dat_proc.R
+++ R/dat_proc.R
@@ -6,18 +6,20 @@ source(here('R/funcs.R'))
 
 # get water quality data from epc ----------------------------------------
 
-epcwq <- epcdata |> 
-  filter(bay_segment == 'OTB') |> 
+epcwq <- epcdata |>
+  filter(bay_segment == 'OTB') |>
   select(-bay_segment)
 
 save(epcwq, file = here('data/epcwq.RData'))
 
 # get loading data -------------------------------------------------------
 
-mosdatraw <- rdataload('https://github.com/tbep-tech/load-estimates/raw/refs/heads/main/data/mosdat.RData') 
+mosdatraw <- rdataload(
+  'https://github.com/tbep-tech/load-estimates/raw/refs/heads/main/data/mosdat.RData'
+)
 
-lddat <- mosdatraw |> 
-  filter(bay_segment == 'Old Tampa Bay') |> 
+lddat <- mosdatraw |>
+  filter(bay_segment == 'Old Tampa Bay') |>
   select(-bay_segment)
 
 save(lddat, file = here('data/lddat.RData'))
@@ -26,7 +28,7 @@ save(lddat, file = here('data/lddat.RData'))
 
 # # get sampling locations for a specific waterbody
 # waterbodies <- util_importwqwa('waterbodies')
-# waterbodyid <- waterbodies |> 
+# waterbodyid <- waterbodies |>
 #   dplyr::filter(grepl('Old Tampa Bay', name)) |>
 #   dplyr::pull(id)
 # samplocs <- util_importwqwa('sampling-locations', waterbodyId = waterbodyid)
